a72d12ce29bbddc68af5a8c4bbba666335a05e16
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-28 02:59:07 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,14 @@
 class Appointment
+  attr_accessor :patient,:doctor
+  @@all = []
+  def initialize(patient,doc,date)
+    @patient = patient
+    @doctor = doc
+    @date = date
+    @@all.push(self)
+  end 
   
-  def initialize
-    
+  def self.all 
+    @@all 
   end 
 end 
